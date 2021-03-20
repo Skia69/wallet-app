a4da7dcc99fd0cb5d34fbfb7552ef80dd675991b
Skia69 <Skiastep@gmail.com>
2021-03-20 13:54:43 +0200

    feat: configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: { enabled: true, content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
