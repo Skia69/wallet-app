9a4e2e29ffa1c177c91f97bbd1eb695d36fd0fd1
Skia69 <Skiastep@gmail.com>
2021-03-20 14:01:45 +0200

    style: barrel export

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from './Navbar';
+export * from './Sidebar';
+export * from './WalletForm';
+export * from './TransactionForm';
+export * from './TransactionList';
+export * from './Wallet';
